6c41169a2bc72281497987ead0c677ccd135a04b
jpjava <root@gedvap0031ed.dev.cme.com>
2019-10-06 20:42:14 -0500

    this is the 2 commit

--- blockchain.py
+++ blockchain.py
@@ -1,7 +1,10 @@
 blockchain = [1] #make an array
+def getLastValue():
+	return blockchain[-1]
+
 def add_value(tranV):
-	blockchain.append([blockchain[-1], 5.3])
-	print(blockchain)
+	blockchain.append([getLastValue(),tranV])
 
 add_value(3)
 
+print(blockchain)
