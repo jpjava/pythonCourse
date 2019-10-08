11924ace8fc68bcef55bc16834e896e790025f70
jpjava <root@gedvap0031ed.dev.cme.com>
2019-10-08 11:31:18 -0500

    this is the Tue Oct  8 11:31:18 CDT 2019 commit

--- blockchain.py
+++ blockchain.py
@@ -6,7 +6,7 @@ def getLastValue():
 def add_value(tranV, last_value=[1]):
 	blockchain.append([last_value, tranV])
 
-tx_amount = input('Your transaction amount please: ')
+tx_amount = float(input('Your transaction amount please: '))
 add_value(tx_amount)
 add_value(last_value=getLastValue(),tranV=3)
 add_value(10.98, getLastValue())
