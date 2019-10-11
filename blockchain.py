4d6f69f38f18d0657098fcf95a7e615e8eee714b
jpjava <root@gedvap0031ed.dev.cme.com>
2019-10-11 15:54:02 -0500

    this is the Fri Oct 11 15:54:02 CDT 2019 commit

--- blockchain.py
+++ blockchain.py
@@ -1,9 +1,12 @@
+#initializing the blockchain list
 blockchain = [] #make an array
 
 def getLastValue():
+	"""Returns the last value of the current blockchain """
 	return blockchain[-1]
 
 def add_value(tranV, last_value=[1]):
+	"""Append a new value as as the last blockchain fvalue to the blockchain"""
 	blockchain.append([last_value, tranV])
 
 def get_user_input():
@@ -15,4 +18,9 @@ add_value(tx_amount)
 add_value(last_value=getLastValue(),tranV=3)
 add_value(10.98, getLastValue())
 
-print(blockchain)
+#output the blockchain
+for block in blockchain:
+	print('Outputting Block')
+	print(block)
+
+print('Done!')
